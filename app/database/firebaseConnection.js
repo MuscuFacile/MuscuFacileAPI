799a55021c1f023a14e65b8825e6847f969e8dc9
MattPoly <matthieu.cayet@etu.univ-lyon1.fr>
2019-01-11 16:03:02 +0100

    fix travis

--- app/database/firebaseConnection.js
+++ app/database/firebaseConnection.js
@@ -11,11 +11,6 @@ exports.getFirebaseAdminDb = () => {
     serviceAccount.client_email = process.env.FIREBASE_CLIENT_EMAIL;
     serviceAccount.client_id = process.env.FIREBASE_CLIENT_ID;
 
-    console.log(process.env.NODE_ENV);
-    console.log(process.env.FIREBASE_PRIVATE_KEY);
-    console.log(typeof process.env.FIREBASE_PRIVATE_KEY);
-    console.log(process.env.FIREBASE_PRIVATE_KEY.length);
-
     const firebase = admin.initializeApp({ //  inititalisation de l'app firebase admin
         credential: admin.credential.cert(serviceAccount),
         databaseURL: 'https://muscufacile-b6eae.firebaseio.com/'
